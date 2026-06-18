cd4cf8f8baa125c4b18342bad5496cb4129bfe83
vdpallaviwankhede <pallaviwankhede56@gmail.com>
2026-06-18 16:08:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+```javascript
+const cards = document.querySelectorAll(".card");
+
+cards[0].addEventListener("click", () => {
+    alert("Caraka Saṃhitā coming soon");
+});
+
+cards[1].addEventListener("click", () => {
+    alert("Suśruta Saṃhitā coming soon");
+});
+
+cards[2].addEventListener("click", () => {
+    alert("Aṣṭāṅga Hṛdayam coming soon");
+});
+```
 
