eb2087cc59bfadd23c7df833aaf6eaa7bd96f208
vdpallaviwankhede <pallaviwankhede56@gmail.com>
2026-06-18 16:55:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.getElementById("charaka-card")
 .addEventListener("click", function(){
 
-alert("Caraka Saṃhitā section coming soon");
+alert("Caraka-Saṃhitā section coming soon");
 
 });
 
 document.getElementById("sushruta-card")
 .addEventListener("click", function(){
 
-alert("Suśruta Saṃhitā section coming soon");
+alert("Suśruta-Saṃhitā section coming soon");
 
 });
 
 document.getElementById("ashtanga-card")
 .addEventListener("click", function(){
 
-alert("Aṣṭāṅga Hṛdayam section coming soon");
+alert("Aṣṭāṅga-Hṛdayam section coming soon");
 
 });
